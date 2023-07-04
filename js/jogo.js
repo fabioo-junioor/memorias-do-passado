1d55f182dbfaa04d3a423d50124b496112514e46
Fábio Júnior <fj_011@hotmail.com>
2023-07-04 20:22:33 -0300

    add modals, ajustes dados.js

--- js/jogo.js
+++ js/jogo.js
@@ -13,9 +13,6 @@ let total_erros = 0;
 let pts_fim = 0;
 const total_cartas = 4;
 
-localStorage.setItem('total_pontos', total_pontos);
-localStorage.setItem('total_erros', total_erros);
-
 
 function flipCard(){
     if(lockBoard){
@@ -70,10 +67,23 @@ function unflipCards(){
 function checkCod(codigo){
     for(var item in cartas){
         if(cartas[item].codigo == codigo){
+            /*
             console.log(cartas[item].pergunta);
             console.log(cartas[item].resposta);
             console.log(cartas[item].descricao);
             console.log(cartas[item].referencia);
+            */
+            const pergunta = document.querySelector(".descPergunta");
+            const resposta = document.querySelector(".descResposta");
+            const descricao = document.querySelector(".descDescricao");
+            const referencia = document.querySelector(".descReferencia");
+
+            pergunta.textContent = cartas[item].pergunta;
+            resposta.textContent = cartas[item].resposta;
+            descricao.textContent = cartas[item].descricao;
+            referencia.textContent = cartas[item].referencia;
+            
+            $('#modalDesc').modal('show');
             return;
             
         }
@@ -81,7 +91,14 @@ function checkCod(codigo){
 }
 function checkFimPartida(){
     if(pts_fim === (total_cartas/2)){
-        console.log("Fim de jogo ", total_pontos, total_erros);
+        //console.log("Fim de jogo ", total_pontos, total_erros);
+
+        const pontos = document.querySelector(".totalPontos");
+        const erros = document.querySelector(".totalErros");
+        pontos.textContent = total_pontos;
+        erros.textContent = total_erros;
+
+        $('#modalEstats').modal('show');
         return;
 
     }
